99f55e34c5005cd04087d888e63b6aea318043bd
Will Boyd <shazzenbot@gmail.com>
2019-09-04 15:55:04 -0400

    init

---
+++ src/main/java/com/linuxacademy/ccdak/schemaregistry/ProducerMain.java
@@ -0,0 +1,9 @@
+package com.linuxacademy.ccdak.schemaregistry;
+
+public class ProducerMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
